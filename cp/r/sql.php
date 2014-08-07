eec0f9701d0c8c9589b2c39b9fefb4769da657d5
ijmachado <noztalgik666@gmail.com>
2014-08-07 18:56:26 -0300

    dsa

--- cp/r/sql.php
+++ cp/r/sql.php
@@ -1,8 +1,8 @@
 <?
-$db_server="MYSQL.silenzio.com.ar";
-$db_usr="silenzio";
-$db_psw="silen123";
-$db_name="web";
+$db_server="localhost";
+$db_usr="zignacom_silen";
+$db_psw="zibeigfrgna";
+$db_name="zignacom_silenzioMarket";
 
 $dbh = mysql_connect ($db_server, $db_usr, $db_psw) or die ('I cannot connect to the database because: ' . mysql_error()); mysql_select_db ($db_name);
 ?>
